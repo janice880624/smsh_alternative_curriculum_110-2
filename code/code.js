1565af67b04a83d4080160300625d12ef5fcabf5
janicekuo <janice880624@gmail.com>
2022-04-18 17:10:46 +0800

    0418

---
+++ code/code.js
@@ -0,0 +1,202 @@
+function MainClick (){
+  console.log("web 被點擊")
+  swal({
+    title: "小組網站 Demo",
+    text: "請選擇瀏覽模式",
+    icon: "success",
+    buttons: {
+      Btn: false,
+      first: {
+        text: "Web",
+        visible: true
+      },
+      second: {
+        text: "Code",
+        visible: true
+      }
+    }
+  }).then((value) => {
+    if (value === "first") {
+      window.location.href =
+        "https://smsh-newtech.github.io/maincode/";
+    }
+    if (value === "second") {
+      window.location.href =
+        "https://github.com/smsh-newtech/maincode";
+    }
+  });
+}
+
+function ATeamClick (){
+  console.log("web 被點擊")
+  swal({
+    title: "A Team 網站",
+    text: "請選擇瀏覽模式",
+    icon: "success",
+    buttons: {
+      Btn: false,
+      first: {
+        text: "Web",
+        visible: true
+      },
+      second: {
+        text: "Code",
+        visible: true
+      }
+    }
+  }).then((value) => {
+    if (value === "first") {
+      window.location.href =
+        "https://smsh-newtech.github.io/teamA/";
+    }
+    if (value === "second") {
+      window.location.href =
+        "https://github.com/smsh-newtech/teamA";
+    }
+  });
+}
+
+function BTeamClick (){
+  console.log("web 被點擊")
+  swal({
+    title: "B Team 網站",
+    text: "請選擇瀏覽模式",
+    icon: "success",
+    buttons: {
+      Btn: false,
+      first: {
+        text: "Web",
+        visible: true
+      },
+      second: {
+        text: "Code",
+        visible: true
+      }
+    }
+  }).then((value) => {
+    if (value === "first") {
+      window.location.href =
+        "https://smsh-newtech.github.io/teamB/";
+    }
+    if (value === "second") {
+      window.location.href =
+        "https://github.com/smsh-newtech/teamB";
+    }
+  });
+}
+
+function CTeamClick (){
+  console.log("web 被點擊")
+  swal({
+    title: "C Team 網站",
+    text: "請選擇瀏覽模式",
+    icon: "success",
+    buttons: {
+      Btn: false,
+      first: {
+        text: "Web",
+        visible: true
+      },
+      second: {
+        text: "Code",
+        visible: true
+      }
+    }
+  }).then((value) => {
+    if (value === "first") {
+      window.location.href =
+        "https://smsh-newtech.github.io/teamC/";
+    }
+    if (value === "second") {
+      window.location.href =
+        "https://github.com/smsh-newtech/teamC";
+    }
+  });
+}
+
+function DTeamClick (){
+  console.log("web 被點擊")
+  swal({
+    title: "D Team 網站",
+    text: "請選擇瀏覽模式",
+    icon: "success",
+    buttons: {
+      Btn: false,
+      first: {
+        text: "Web",
+        visible: true
+      },
+      second: {
+        text: "Code",
+        visible: true
+      }
+    }
+  }).then((value) => {
+    if (value === "first") {
+      window.location.href =
+        "https://smsh-newtech.github.io/teamD/";
+    }
+    if (value === "second") {
+      window.location.href =
+        "https://github.com/smsh-newtech/teamD";
+    }
+  });
+}
+
+function ETeamClick (){
+  console.log("web 被點擊")
+  swal({
+    title: "E Team 網站",
+    text: "請選擇瀏覽模式",
+    icon: "success",
+    buttons: {
+      Btn: false,
+      first: {
+        text: "Web",
+        visible: true
+      },
+      second: {
+        text: "Code",
+        visible: true
+      }
+    }
+  }).then((value) => {
+    if (value === "first") {
+      window.location.href =
+        "https://smsh-newtech.github.io/teamE/";
+    }
+    if (value === "second") {
+      window.location.href =
+        "https://github.com/smsh-newtech/teamE";
+    }
+  });
+}
+
+function FTeamClick (){
+  console.log("web 被點擊")
+  swal({
+    title: "F Team 網站",
+    text: "請選擇瀏覽模式",
+    icon: "success",
+    buttons: {
+      Btn: false,
+      first: {
+        text: "Web",
+        visible: true
+      },
+      second: {
+        text: "Code",
+        visible: true
+      }
+    }
+  }).then((value) => {
+    if (value === "first") {
+      window.location.href =
+        "https://smsh-newtech.github.io/teamF/";
+    }
+    if (value === "second") {
+      window.location.href =
+        "https://github.com/smsh-newtech/teamF";
+    }
+  });
+}
